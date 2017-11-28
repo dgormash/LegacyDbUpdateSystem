79af489fe877ed194e4d7c847599127aed61f0ec
D. Gormash <dlgormash@gmail.com>
2017-11-28 23:17:02 +0500

    Заполняю сборку FileSystemManager

--- FileSystemManager/FileManager.cs
+++ FileSystemManager/FileManager.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace FileSystemManager
 {
-    public class Class1
+    public class FileManager
     {
+        private const string RegionCode = "75";
     }
 }
---
+++ FileSystemManager/Interfaces/IFileManager.cs
@@ -0,0 +1,9 @@
+﻿namespace FileSystemManager.Interfaces
+{
+    public interface IFileManager
+    {
+        string AssembleFileName();
+        bool CreateFile(string path);
+        bool WriteDataToFile(string path, object data);
+    }
+}
