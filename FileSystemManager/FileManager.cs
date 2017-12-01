165ecfbcd5a15c0c770554bf3064e0e101af71fb
D. Gormash <dlgormash@gmail.com>
2017-12-01 17:03:07 +0500

    Прописал первоначальные интерфейсы класса FileManager

--- FileSystemManager/FileManager.cs
+++ FileSystemManager/FileManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FileSystemManager.Interfaces;
 
 namespace FileSystemManager
 {
@@ -10,14 +11,14 @@ namespace FileSystemManager
     {
         private const string RegionCode = "75";
         //FileManager устанавливает поле <имя файла> в формате 75<v|f>NNNNNN.add
-        private IFileNameSetter _fileNameSetter;
+        private IFileNameAssembler _fileNameAssembler;
         private string _fileName;
         //FileManager создаёт файл с указанным именем и записывает в него информацию
         private IFileDataWriter _fileDataWriter;
         //FileManager упаковывает файл внешним архиватором
         private IArchiver _archiver;
         //FileManager вычисляет контрольную сумму архива (MD5)
-        private IMD5Calculator _md5Calculator;
+        private IMd5Calculator _md5Calculator;
         //FileManager выгружает архив на указаннный ftp-ресурс
         private IFtpUploader _ftpUploader;
         //FileManager проверяет корректность выгрузки
